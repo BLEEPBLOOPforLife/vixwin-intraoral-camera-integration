e03230e928ee437e40c34a8f36a16ffb0a27d256
Brian Mak <eggrollbot@gmail.com>
2020-11-24 23:07:16 -0800

    Initial commit

---
+++ VixWinIntraoralCameraIntegration/TrayIcon.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VixWinIntraoralCameraIntegration
+{
+	/// <summary>
+	/// The tray icon class.
+	/// </summary>
+	public class TrayIcon
+	{
+		/// <summary>
+		/// Gets the current instance of the tray icon.
+		/// </summary>
+		public static TrayIcon Current { get; private set; }
+		private readonly NotifyIcon trayIconMenu;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="App"/> class.
+		/// </summary>
+		public TrayIcon( )
+		{
+			Current = this;
+			trayIconMenu = new NotifyIcon( );
+			trayIconMenu.Icon = new Icon( AppDomain.CurrentDomain.BaseDirectory + "tray_icon.ico" );
+			trayIconMenu.Text = "VixWin Intraoral Camera Integration";
+			trayIconMenu.ContextMenuStrip = new ContextMenuStrip( );
+			trayIconMenu.Visible = true;
+			PopulateTrayIconMenu( );
+		}
+
+		/// <summary>
+		/// Selects a webcam for displaying in the tray menu.
+		/// </summary>
+		/// <param name="cameraName">The webcam's pretty name.</param>
+		internal void SelectWebcamForDisplay( string webcamName )
+		{
+			trayIconMenu.ContextMenuStrip.Items[ 2 ].Text = "Camera: " + webcamName;
+		}
+
+		/// <summary>
+		/// Updates the webcam list in the sub-context menu for webcam selection.
+		/// </summary>
+		/// <param name="selectWebcamMenu">The webcam sub-context menu to update.</param>
+		private void UpdateWebcamList( ToolStripMenuItem selectWebcamMenu )
+		{
+			ToolStripItemCollection dropDownItems = selectWebcamMenu.DropDownItems;
+			dropDownItems.Clear( );
+
+			foreach ( KeyValuePair<string, string> webcam in WebcamManager.GetWebcamList( ) )
+			{
+				dropDownItems.Add( webcam.Value, null, ( object sender, EventArgs handler ) => WebcamManager.SelectWebcam( webcam.Key ) );
+			}
+		}
+
+		/// <summary>
+		/// Populates the tray icon menu initially.
+		/// </summary>
+		private void PopulateTrayIconMenu( )
+		{
+			ToolStripItemCollection trayItems = trayIconMenu.ContextMenuStrip.Items;
+			trayItems.Add( new ToolStripLabel( "VixWin Intraoral Camera Integration" ) );
+			trayItems.Add( new ToolStripSeparator( ) );
+			trayItems.Add( new ToolStripLabel( "" ) );
+			trayItems.Add( new ToolStripSeparator( ) );
+			ToolStripMenuItem selectWebcamMenu = new ToolStripMenuItem( "Select Webcam" );
+
+			trayIconMenu.MouseClick += ( object sender, MouseEventArgs eventArgs ) =>
+			{
+				if ( eventArgs.Button == MouseButtons.Right )
+				{
+					UpdateWebcamList( selectWebcamMenu );
+				}
+			};
+
+			trayItems.Add( selectWebcamMenu );
+			trayItems.Add( new ToolStripSeparator( ) );
+			trayItems.Add( "Exit", null, App.Current.Exit );
+		}
+	}
+}
---
+++ VixWinIntraoralCameraIntegration/WebcamManager.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+using Accord.Video;
+using Accord.Video.DirectShow;
+
+namespace VixWinIntraoralCameraIntegration
+{
+	/// <summary>
+	/// The class that manages the active webcam.
+	/// </summary>
+	public static class WebcamManager
+	{
+		/// <summary>
+		/// Gets the currently selected video source.
+		/// </summary>
+		internal static VideoCaptureDevice VideoSource { get; private set; }
+		private static DateTime firstFrameDateTime;
+
+		/// <summary>
+		/// Gets the last frame captured.
+		/// </summary>
+		public static Bitmap LastFrame { get; private set; }
+		private readonly static Dictionary<string, string> webcamDictionary;
+
+		/// <summary>
+		/// Static constructor for the <see cref="WebcamManager"/> class.
+		/// </summary>
+		static WebcamManager( )
+		{
+			webcamDictionary = new Dictionary<string, string>( );
+		}
+
+		/// <summary>
+		/// Gets a read-only dictionary of webcams.
+		/// </summary>
+		/// <returns>A ReadOnlyDictionary with the key being the camera's moniker and the value being the camera's pretty name.</returns>
+		public static ReadOnlyDictionary<string, string> GetWebcamList( )
+		{
+			RefreshWebcamList( );
+
+			return new ReadOnlyDictionary<string, string>( webcamDictionary );
+		}
+
+		/// <summary>
+		/// Switches the active webcam.
+		/// </summary>
+		/// <param name="monikerString">The moniker string of the new webcam.</param>
+		public static void SelectWebcam( string monikerString )
+		{
+			if ( monikerString != null && webcamDictionary.TryGetValue( monikerString, out string webcamPrettyName ) )
+			{
+				App.CurrentVideoDisplayForm.StopVideoStream( );
+				firstFrameDateTime = default;
+				VideoSource = new VideoCaptureDevice( monikerString );
+				VideoSource.NewFrame += new NewFrameEventHandler( NewVideoFrame );
+				VideoSource.ProvideSnapshots = true;
+				VideoSource.SnapshotFrame += new NewFrameEventHandler( NewSnapshotFrame );
+				App.CurrentVideoDisplayForm.SetVideoStream( VideoSource );
+				TrayIcon.Current.SelectWebcamForDisplay( webcamPrettyName );
+				File.WriteAllText( AppDomain.CurrentDomain.BaseDirectory + "default_webcam.txt", monikerString );
+			} else
+			{
+				App.CurrentVideoDisplayForm.StopVideoStream( );
+				VideoSource = null;
+				TrayIcon.Current.SelectWebcamForDisplay( "None" );
+			}
+		}
+
+		/// <summary>
+		/// Refreshes the webcam dictionary.
+		/// </summary>
+		internal static void RefreshWebcamList( )
+		{
+			webcamDictionary.Clear( );
+			FilterInfoCollection videoDevices = new FilterInfoCollection( FilterCategory.VideoInputDevice );
+
+			foreach ( FilterInfo videoDevice in videoDevices )
+			{
+				webcamDictionary.Add( videoDevice.MonikerString, videoDevice.Name );
+			}
+		}
+
+		/// <summary>
+		/// Called when a new video frame is sent to us.
+		/// </summary>
+		private static void NewVideoFrame( object sender, NewFrameEventArgs eventArgs )
+		{
+			if ( firstFrameDateTime.Equals( default ) )
+			{
+				firstFrameDateTime = DateTime.Now;
+			}
+
+			Bitmap frame = eventArgs.Frame;
+			LastFrame?.Dispose( );
+			LastFrame = new Bitmap( frame );
+		}
+
+		/// <summary>
+		/// Called when a new snapshot frame is sent to us.
+		/// </summary>
+		private static void NewSnapshotFrame( object sender, NewFrameEventArgs eventArgs )
+		{
+			if ( firstFrameDateTime.Equals( default ) )
+			{
+				return;
+			}
+
+			TimeSpan timeSinceFirstFrame = DateTime.Now - firstFrameDateTime;
+
+			if ( timeSinceFirstFrame.TotalSeconds < 0.5 ) // Add this delay because the camera takes a snapshot at the beginning for some reason.
+			{
+				return;
+			}
+
+			Bitmap frame = eventArgs.Frame;
+			string directoryPath = @"C:\VixTemp\FileIn\";
+			string[ ] files = Directory.GetFiles( directoryPath );
+			int indexOfLastFile = -1;
+
+			if ( files.Any( ) )
+			{
+				foreach ( string file in files )
+				{
+					string indexOfFile = file.Replace( directoryPath + "plug", "" ).Replace( ".jpg", "" );
+					bool successfullyConvertedToInt = int.TryParse( indexOfFile, out int indexOfFileAsInt );
+
+					if ( successfullyConvertedToInt && indexOfFileAsInt > indexOfLastFile )
+					{
+						indexOfLastFile = indexOfFileAsInt;
+					}
+				}
+			}
+
+			frame.Save( directoryPath + "plug" + ( indexOfLastFile + 1 ) + ".jpg", ImageFormat.Jpeg );
+		}
+	}
+}
